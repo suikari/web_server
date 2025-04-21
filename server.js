ef96e1f55749c6bb9ff2a3de6aa10987be6e5421
suikari <suikari@naver.com>
2025-04-21 18:18:06 +0900

    25-04-21 서버 관련 커밋

--- server.js
+++ server.js
@@ -33,6 +33,47 @@ app.get('/board/list', async (req, res) => {
     }
 }) 
 
+app.get('/board/view', async (req, res) => {
+    try {
+        const boardno = req.query.boardno;  // 쿼리 파라미터 받기
+
+        if (!boardno) {
+            return res.status(400).json({ message: "boardno가 필요합니다." });
+        }
+        
+        const [list] = await db.query("SELECT * FROM BOARD WHERE BOARDNO  = ?", [boardno]);
+
+        res.json({
+            message: "호출 성공",
+            list: list[0]  //
+        });
+
+    } catch(err) {
+        console.log('에러 발생!');
+        res.status(500).send( 'Server Error');
+    }
+}) 
+
+app.get('/board/delete', async (req, res) => {
+    try {
+        const boardno = req.query.boardno;  // 쿼리 파라미터 받기
+
+        if (!boardno) {
+            return res.status(400).json({ message: "boardno가 필요합니다." });
+        }
+        
+        const result = await db.query("DELETE FROM BOARD WHERE BOARDNO  = ?", [boardno]);
+
+        res.json({
+            message: "삭제 성공"
+        });
+
+    } catch(err) {
+        console.log('에러 발생!');
+        res.status(500).send( 'Server Error');
+    }
+}) 
+
 app.listen(3000, ()=>{
     console.log('서버 실행 중!');
 });
